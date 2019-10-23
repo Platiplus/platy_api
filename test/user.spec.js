a3ceba3a32b9ed4a22b779041ddefd061b84a14c
Victor Rosa <victor@m2mtelemetria.com.br>
2019-10-23 15:58:01 -0300

    User creation unit test implemented

---
+++ test/user.spec.js
@@ -0,0 +1,47 @@
+/* eslint-env mocha */
+
+// UTILS AND MODELS
+const Database = require('../utils/Database')
+const User = require('../api/models/user-model')
+
+// DEV DEPENDENCIES
+const casual = require('casual')
+const chai = require('chai')
+const expect = chai.expect
+const dirtyChai = require('dirty-chai')
+const chaiHttp = require('chai-http')
+const server = require('../app')
+
+// MIDDLEWARES
+chai.use(chaiHttp)
+chai.use(dirtyChai)
+
+// USER RELATED TESTS
+describe('User', () => {
+  before(async () => {
+    const db = new Database()
+    await db.connect('test')
+    await User.deleteMany({})
+  })
+
+  describe('/POST User Creation', () => {
+    it('it should create an user', (done) => {
+      const mockUser = {
+        username: casual.username,
+        email: casual.email,
+        password: casual.password,
+        initialBalance: Math.random() * (9999 - 1) + 1
+      }
+      chai.request(server)
+        .post('/user/create')
+        .send(mockUser)
+        .end((err, res) => {
+          expect(err).to.be.null()
+          expect(res).to.have.status(201)
+          expect(res.body).to.be.a('object')
+          expect(res.body).to.have.property('message').equal('User Created succesfully!')
+          done()
+        })
+    })
+  })
+})
