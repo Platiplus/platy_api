f6d5609050dac118f4c509fb9ce201afcba90b29
Victor Rosa <victor@m2mtelemetria.com.br>
2019-11-14 13:29:18 -0200

    Normalize Date implemented

--- utils/Utils.js
+++ utils/Utils.js
@@ -1,8 +1,18 @@
+const moment = require('moment')
+
 class Utils {
   chaoticInputGenerator (target) {
     var properties = Object.keys(target)
     delete target[properties[properties.length * Math.random() << 0]]
     return target
+  }
+
+  normalizeDate (date) {
+    const parsedDate = moment(date, 'DD/MM/YYYY')
+    if (parsedDate.isValid()) {
+      return parsedDate.format('YYYY-MM-DD')
+    }
+    return undefined
   }
 }
 
